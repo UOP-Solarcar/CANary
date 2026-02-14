48f9df932f53a157abc8482d17553c712658b537
smcc977 <sammcc977@gmail.com>
2026-02-13 16:13:46 -0800

    Redo fork

--- application/parent.py
+++ application/parent.py
@@ -1,10 +1,15 @@
 import os
 import serialRcv
 import dashboard
+import multiprocessing
 
 if __name__ == '__main__':
-    pid = os.fork()
-    if (pid > 0):
-        os.system("streamlit run application/dashboard.py")
-    else:
-        serialRcv.serialRcv()
+    multiprocessing.set_start_method("spawn", force=True) #
+    p = multiprocessing.Process(target=serialRcv.serialRcv())
+    p.start()
+    os.system("streamlit run application/dashboard.py")
+    p.join()
+    #if (pid > 0):
+    #    os.system("streamlit run application/dashboard.py")
+    #else:
+    #    serialRcv.serialRcv()
