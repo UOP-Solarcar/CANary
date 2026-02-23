41e2b47185f9a8b191754d9f2b956a7715d4e995
NicholasH15 <n_henricksen@u.pacific.edu>
2026-02-23 15:22:39 -0800

    Fixed serial receiver for data format

--- application/serialRcv.py
+++ application/serialRcv.py
@@ -1,5 +1,6 @@
 import serial
 import os
+from enum import Enum 
 
 if (os.name == "posix"): # sets the serial port
     PORT = "/dev/tty.usbmodem21101"
@@ -8,8 +9,14 @@ elif (os.name == "nt"):
 
 BAUD = 115200          # rate of transmission in bps
 
-STX = 0x0A             # start of serial frame
-ETX = 0x0A            # end of serial frame
+class MESSAGE_ID(Enum):
+    BASIC = 0x6B0
+    BMS_TEMP = 0x6B1
+    STRINGS = 0x6B2
+    BATTERY_TEMP = 0x6B3
+    HEALTH = 0x6B4
+    CELL = 0x36
+
 
 ser = serial.Serial(PORT, BAUD, timeout=1)
 
@@ -19,25 +26,26 @@ def read_frame(ser):
         b = ser.read(1)
         if not b:
             return None
-        if b[0] == STX:
+        try:
+            MESSAGE_ID(b)
             break
+        except ValueError:
+            pass    # suppress error output 
 
-    # Collect payload until ETX
+    # Collect 8 byte payload
     payload = bytearray()
-    while True:
+    for i in range(8):
         b = ser.read(1)
         if not b:
             return None
-        if b[0] == ETX:
-            return bytes(payload)
         payload.append(b[0])
+    return payload
 
 def serialRcv():
     while True:
         frame = read_frame(ser)
-        if frame is not None:
-            #print("Received frame:", frame.decode(encoding='utf-8'))
-            None
+        if frame:
+            print("Received frame:", frame.decode(encoding='utf-8'))
         else:
             print("No data in frame")
 
