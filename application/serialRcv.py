0942a9e3081ba1644ea10772ae631c059a392784
NicholasH15 <n_henricksen@u.pacific.edu>
2026-02-25 16:08:27 -0800

    Serial Receiver v3

--- application/serialRcv.py
+++ application/serialRcv.py
@@ -10,7 +10,7 @@ elif (os.name == "nt"):
 BAUD = 115200          # rate of transmission in bps
 
 class MESSAGE_ID(Enum):
-    BASIC = 0x6B0
+    BASIC = 0x6B0   # in Python Hex value is just another representation of integer. So can do operation on value as if it is integer
     BMS_TEMP = 0x6B1
     STRINGS = 0x6B2
     BATTERY_TEMP = 0x6B3
@@ -21,16 +21,28 @@ class MESSAGE_ID(Enum):
 ser = serial.Serial(PORT, BAUD, timeout=1)
 
 def read_frame(ser):
-    # Wait for start byte
+    VALID_IDS = {m.value for m in MESSAGE_ID}
+    
+    # Wait for 4 byte Message ID
+    buffer = bytearray()
     while True:
         b = ser.read(1)
         if not b:
             return None
-        try:
-            MESSAGE_ID(b)
-            break
-        except ValueError:
-            pass    # suppress error output 
+        '''
+        append requires integer, so this converts byte to integer representation,
+        and then converts it back into bytes object when appending
+        '''
+        buffer.append(b[0])
+
+        # Keep only last 4 bytes (sliding window)
+        if len(buffer) > 4:
+            buffer.pop(0)
+        
+        if len(buffer) == 4:
+            msg_id = int.from_bytes(buffer, byteorder='big')
+            if msg_id in VALID_IDS:
+                break
 
     # Collect 8 byte payload
     payload = bytearray()
@@ -39,13 +51,14 @@ def read_frame(ser):
         if not b:
             return None
         payload.append(b[0])
-    return payload
+    return (hex(msg_id), payload)
 
 def serialRcv():
     while True:
         frame = read_frame(ser)
         if frame:
-            print("Received frame:", frame.decode(encoding='utf-8'))
+            print("Received frame --> MSG_ID: ", frame[0], ": ", frame[1].hex())
+            #print("Received frame:", frame.decode(encoding='utf-8'))
         else:
             print("No data in frame")
 
--- serialtest/src/main.cpp
+++ serialtest/src/main.cpp
@@ -1,13 +1,25 @@
 #include <Arduino.h>
 
-int i;
+uint8_t msg[12];
 
 void setup() {
-  i = 0;
   Serial.begin(115200);
+  msg[0] = 0x00;
+  msg[1] = 0x00;
+  msg[2] = 0x06;
+  msg[3] = 0xB0;
+  msg[4] = 0xD8;
+  msg[5] = 0x03;
+  msg[6] = 0xB6;
+  msg[7] = 0x4E;
+  msg[8] = 0x4B;
+  msg[9] = 0x00;
+  msg[10] = 0x03;
+  msg[11] = 0xA1;
 }
 
 void loop() {
-  i++;
-  Serial.println(i);
+  for (int i = 0; i < 12; ++i) {
+     Serial.write(msg, 12);
+  }
 }
