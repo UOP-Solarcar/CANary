2c77e040f3f59f6b2792f96c3d3fe09639c42b04
NicholasH15 <n_henricksen@u.pacific.edu>
2026-03-04 16:11:30 -0800

    Naturalized data and write to csv

--- application/serialRcv.py
+++ application/serialRcv.py
@@ -61,34 +61,91 @@ def naturalizeData(msg_id, data):
     if ID == 'BASIC':
         pack_current = int.from_bytes(data[0:2], signed=True, byteorder='big')  # signed int, have to specify so method performs two's comp to decode
         pack_inst_voltage = int.from_bytes(data[2:4], signed=False, byteorder='big')
-        pack_soc = int.from_bytes(data[4], signed=False, byteorder='big')
+        pack_soc = int.from_bytes(data[4:5], signed=False, byteorder='big')
         relay_state = int.from_bytes(data[5:7], signed=False, byteorder='big')
-        checksum = int.from_bytes(data[7], signed=False, byteorder='big')
-        return [pack_current, pack_inst_voltage, pack_soc, relay_state, checksum]
+        checksum = int.from_bytes(data[7:], signed=False, byteorder='big')
+        return ['BASIC', pack_current, pack_inst_voltage, pack_soc, relay_state, checksum]
     elif ID == 'BMS_TEMP':
         pack_dcl = int.from_bytes(data[0:2], signed=False, byteorder='big')
         pack_ccl = int.from_bytes(data[2:4], signed=False, byteorder='big')
-        high_temp = int.from_bytes(data[4], signed=False, byteorder='big')
-        low_temp = int.from_bytes(data[5], signed=False, byteorder='big')
-        checksum = int.from_bytes(data[6], signed=False, byteorder='big')
+        high_temp = int.from_bytes(data[4:5], signed=False, byteorder='big')
+        low_temp = int.from_bytes(data[5:6], signed=False, byteorder='big')
+        checksum = int.from_bytes(data[6:], signed=False, byteorder='big')
+        return ['BMS_TEMP', pack_dcl, pack_ccl, high_temp, low_temp, checksum]
     elif ID == 'STRINGS':
         high_cell_voltage = int.from_bytes(data[0:2], signed=False, byteorder='big')
-        high_cell_voltage_id = int.from_bytes(data[2], signed=False, byteorder='big')
+        high_cell_voltage_id = int.from_bytes(data[2:3], signed=False, byteorder='big')
         low_cell_voltage = int.from_bytes(data[3:5], signed=False, byteorder='big')
-        low_cell_voltage_id = int.from_bytes(data[5], signed=False, byteorder='big')
-        checksum = int.from_bytes(data[6], signed=False, byteorder='big')
+        low_cell_voltage_id = int.from_bytes(data[5:6], signed=False, byteorder='big')
+        checksum = int.from_bytes(data[6:], signed=False, byteorder='big')
+        return ['STRINGS', high_cell_voltage, high_cell_voltage_id, low_cell_voltage, low_cell_voltage_id, checksum]
+    elif ID == 'BATTERY_TEMP':
+        high_temp = int.from_bytes(data[0:1], signed=False, byteorder='big')
+        high_thermistor_id = int.from_bytes(data[1:2], signed=False, byteorder='big')
+        low_temp = int.from_bytes(data[2:3], signed=False, byteorder='big')
+        low_thermistor_id = int.from_bytes(data[3:4], signed=False, byteorder='big')
+        avg_temp = int.from_bytes(data[4:5], signed=False, byteorder='big')
+        internal_temp = int.from_bytes(data[5:6], signed=False, byteorder='big')
+        checksum = int.from_bytes(data[6:], signed=False, byteorder='big')
+        return ['BATTERY_TEMP', high_temp, high_thermistor_id, low_temp, low_thermistor_id, avg_temp, internal_temp, checksum]
+    elif ID == 'HEALTH':
+        pack_health = int.from_bytes(data[0:1], signed=False, byteorder='big')
+        adaptive_total_capacity = int.from_bytes(data[1:3], signed=False, byteorder='big')
+        input_supply_voltage = int.from_bytes(data[3:5], signed=False, byteorder='big')
+        checksum = int.from_bytes(data[5:], signed=False, byteorder='big')
+        return ['HEALTH', pack_health, adaptive_total_capacity, input_supply_voltage, checksum]
+    elif ID == 'CELL':
+        cell_id = int.from_bytes(data[0:1], signed=False, byteorder='big')
+        instant_voltage = int.from_bytes(data[1:3], signed=False, byteorder='big')
+        internal_resistance = int.from_bytes(data[3:5], signed=False, byteorder='big')
+        open_voltage = int.from_bytes(data[5:7], signed=False, byteorder='big')
+        checksum = int.from_bytes(data[7:], signed=False, byteorder='big')
+        return ['CELL', cell_id, instant_voltage, internal_resistance, open_voltage, checksum]
+    print("ERROR TRANSLATING DATA")
+    return
+
+def writeData(actual_data):
+    '''f = open('data.csv', 'a')
+    for _ in actual_data[1:]:
+    match actual_data[0]:
+        case 'BASIC':
+            for _ in actual_data[1:]:
+        case 'BMS_TEMP':
+        case 'STRINGS':
+        case 'BATTERY_TEMP':
+        case 'HEALTH':
+        case 'CELL': '''
 
 
 def serialRcv():
+    f = open('data.csv', 'w')
+    f.write("BASIC,pack_current,pack_inst_voltage,pack_soc,relay_state,checksum," \
+           "BMS_TEMP,pack_dcl,pack_ccl,high_temp,low_temp,checksum," \
+           "STRINGS,high_cell_voltage,high_cell_voltage_id,low_cell_voltage,low_cell_voltage_id,checksum," \
+           "BATTERY_TEMP,high_temp,high_thermistor_id,low_temp,low_thermistor_id,avg_temp,internal_temp,checksum," \
+           "HEALTH,pack_health,adaptive_total_capacity,input_supply_voltage,checksum" \
+           "CELL,cell_id,instant_voltage,internal_resistance,open_voltage,checksum\n")
+    f.close()
+    
+    
+    count = 0
     while True:
         frame = read_frame(ser)
         if frame:
             print("Received frame --> MSG_ID: ", frame[0][2:].hex(), ": ", frame[1].hex())
-            #naturalizeData(frame[0], frame[1])
-            #print("Received frame:", frame.decode(encoding='utf-8'))
+            print(frame[1])
+            actual_data = naturalizeData(frame[0], frame[1])
             f = open('data.csv', 'a')
-            count = 0
-            for byte in frame[0]:
+            f.write(actual_data[0] + ',')
+            for data in actual_data[1:]:
+                f.write(str(data) + ',')
+            count = count + 1
+            if (count == 5):
+                f.write('\n')
+                count = 0
+
+            
+            '''for byte in frame[0]:
                 if count == 3:
                     f.write(str(byte))
                 f.write(str(byte) + ',')
@@ -100,7 +157,7 @@ def serialRcv():
                 else:
                     f.write(str(byte) + ',')
                     count = count + 1
-            f.write('\n')
+            f.write('\n')'''
             f.close()
         else:
             print("No data in frame")
